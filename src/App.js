ad0b772094822503c52733c40d29517b96d77405
Damián López <lopezrunco@gmail.com>
2022-07-09 22:07:42 +0530

    User state managment, apiUrl util & Login component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useReducer, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { HIDE_LOADER, LOGIN, LOGOUT, REFRESH_TOKEN, SHOW_LOADER } from './action-types'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Pages
+import Login from './pages/Login'
+
+export const AuthContext = createContext()
+
+const initialState = {
+  isAuthenticated: !!localStorage.getItem('token'),
+  user: JSON.parse(localStorage.getItem('user')),
+  role: localStorage.getItem('role'),
+  token: localStorage.getItem('token'),
+  refreshToken: localStorage.getItem('refreshToken'),
+  showingLoader: false
 }
 
-export default App;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case LOGIN:
+      localStorage.setItem('user', JSON.stringify(action.payload.user))
+      localStorage.setItem('role', action.payload.user.role)
+      localStorage.setItem('token', action.payload.user.token)
+      localStorage.setItem('refreshToken', action.payload.user.refreshToken)
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+        role: action.payload.user.role,
+        token: action.payload.user.token,
+        refreshToken: action.payload.user.refreshToken
+      }
+
+    case REFRESH_TOKEN:
+      localStorage.setItem('token', action.payload.token)
+      localStorage.setItem('refreshToken', action.payload.refreshToken)
+      return {
+        ...state,
+        token: action.payload.token,
+        refreshToken: action.payload.refreshToken
+      }
+
+    case LOGOUT:
+      localStorage.clear()
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+        role: null,
+        token: null,
+        refreshToken: null
+      }
+
+    case SHOW_LOADER:
+      return {
+        ...state,
+        showingLoader: true
+      }
+
+    case HIDE_LOADER:
+      return {
+        ...state,
+        showingLoader: false
+      }
+
+    default:
+      return state
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'))
+    const role = localStorage.getItem('role')
+    const token = localStorage.getItem('token')
+    // If the user is loged in, dispatch the user data
+    if (user && token) {
+      dispatch({
+        type: LOGIN,
+        payload: {
+          user,
+          role,
+          token
+        }
+      })
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }} >
+      <div className="App">
+        <p>Hi world!</p>
+        <Routes>
+
+          <Route path='/login' element={
+            <Login />
+          } />
+
+        </Routes>
+      </div>
+    </AuthContext.Provider>
+  )
+}
+
+export default App
---
+++ src/pages/Login/index.js
@@ -0,0 +1,104 @@
+import { useContext, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../App"
+import { LOGIN } from "../../action-types"
+import { apiUrl } from '../../utils/api-url'
+
+function Login() {
+    const { dispatch } = useContext(AuthContext)
+    const navigate = useNavigate()
+    const initialState = {
+        email: '',
+        password: '',
+        token: '',
+        isSubmitting: false,
+        errorMessage: null
+    }
+    const [data, setData] = useState(initialState)
+
+    const handleInputChange = event => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleFormSubmit = () => {
+        setData({
+            ...data,
+            isSubmitting: true,
+            errorMessage: null
+        })
+        fetch(apiUrl('login'), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: data.email,
+                password: data.password,
+                token: data.token
+            })
+        }).then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw response
+            }
+        }).then(data => {
+            dispatch({
+                type: LOGIN,
+                payload: data
+            })
+            navigate('/home')
+        }).catch(error => {
+            console.error(error)
+            setData({
+                ...data,
+                isSubmitting: false,
+                errorMessage: 'Credenciales inválidas'
+            })
+        })
+    }
+
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-12'>
+                    <h2>Login</h2>
+                </div>
+            </div>
+            <label htmlFor='email'>
+                Email
+                <input
+                    type='text'
+                    value={data.email}
+                    onChange={handleInputChange}
+                    name='email'
+                    id='email'
+                />
+            </label>
+            <label htmlFor='password'>
+                Password
+                <input
+                    type='text'
+                    value={data.password}
+                    onChange={handleInputChange}
+                    name='password'
+                    id='password'
+                />
+            </label>
+
+            <button onClick={handleFormSubmit} disabled={data.isSubmitting}>
+                {data.isSubmitting ? ('Espere...') : ('Iniciar sesión')}
+            </button>
+            {data.errorMessage && (<span>{data.errorMessage}</span>)}
+            <div>
+                <p>¿Aún no tienes una cuenta? <Link to='/register'>Regístrate</Link></p>
+                <Link to='/'>Volver a Inicio</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Login
